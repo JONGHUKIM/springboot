0fdb74487843913860be15b04b2e4d07ca439711
JONGHUKIM <hotsky803@gmail.com>
2025-02-17 15:48:17 +0900

    domain

---
+++ springboot1/src/main/java/com/itwill/springboot1/domain/book.java
@@ -0,0 +1,19 @@
+package com.itwill.springboot1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class book {
+	
+	private Integer id;
+	private String title;
+	private Author author;
+	
+
+}
