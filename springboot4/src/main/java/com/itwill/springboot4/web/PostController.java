9859defb2db26c824e71aa4eed112bceeaa2dee6
JONGHUKIM <hotsky803@gmail.com>
2025-02-26 17:29:04 +0900

    .

--- springboot4/src/main/java/com/itwill/springboot4/web/PostController.java
+++ springboot4/src/main/java/com/itwill/springboot4/web/PostController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.itwill.springboot4.domain.Post;
 import com.itwill.springboot4.dto.PostCreateDto;
 import com.itwill.springboot4.dto.PostListItemDto;
+import com.itwill.springboot4.dto.PostSearchDto;
 import com.itwill.springboot4.dto.PostUpdateDto;
 import com.itwill.springboot4.service.PostService;
 
@@ -24,69 +24,82 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/post")
 public class PostController {
-	
+
 	private final PostService postService;
-	
+
 	@GetMapping("/list")
 	public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) {
 		log.info("list(pageNo={})", pageNo);
-		
+
 		// id 필드의 내림차순으로 정렬된 페이지 데이터를 가져옴.
 		Page<PostListItemDto> page = postService.read(pageNo, Sort.by("id").descending());
-		
 		model.addAttribute("page", page);
+		
+		// pagination 프래그먼트의 링크(버튼)의 요청 주소로 사용할 문자열을 모델 속성으로 저장
+		model.addAttribute("baseUrl", "/post/list");
 	}
-	
+
 	@GetMapping("/create")
 	public void create() {
 		log.info("GET create()");
 	}
-	
+
 	@PostMapping("/create")
 	public String create(PostCreateDto dto) {
 		log.info("POST create(dto={})", dto);
-		
+
 		Long id = postService.create(dto);
 		log.info("저장된 엔터티 id = {}", id);
-		
+
 		// 포스트 목록 페이지로 이동(redirect)
 		return "redirect:/post/list";
 	}
-	
-	@GetMapping({"/details", "/modify"})
+
+	@GetMapping({ "/details", "/modify" })
 	public void details(Long id, Model model) {
 		log.info("details(id={})", id);
-		
+
 		// 서비스 메서드 호출, 아이디로 검색
 		Post entity = postService.read(id);
 		model.addAttribute("post", entity);
-		
+
 		// 리턴 타입이 void이기 때문에 요청 주소에 따라서 뷰의 이름이 달라짐
 		// 요청 주소가 details인 경우는 details.html
 		// 요청 주소가 modify인 경우는 mnodify.html
 	}
-	
+
 	@GetMapping("/delete")
 	public String delete(Long id) {
 		log.info("delete(id={})", id);
-		
+
 		postService.delete(id);
-		
+
 		return "redirect:/post/list";
-		
+
 	}
-	
+
 	@PostMapping("/update")
 	public String update(PostUpdateDto dto) {
 		log.info("update(dto={})", dto);
-		
+
 		postService.update(dto);
 		log.info("업데이트된 엔터티 id = {}", dto.getId());
-		
-		return "redirect:/post/details?id="+ dto.getId();
-		
+
+		return "redirect:/post/details?id=" + dto.getId();
+
 	}
-	
-	
+
+	@GetMapping("/search")
+	public String search(PostSearchDto dto, Model model) {
+		log.info("search(dto={})", dto);
+		
+		Page<PostListItemDto> page = postService.search(dto, Sort.by("id").descending());
+		model.addAttribute("page", page);
+		
+		// pagination 프래그먼트의 링크(버튼)의 요청 주소로 사용할 문자열을 모델 속성으로 저장
+		model.addAttribute("baseUrl", "/post/search");
+		
+		return "post/list";
+	}
 
 }
