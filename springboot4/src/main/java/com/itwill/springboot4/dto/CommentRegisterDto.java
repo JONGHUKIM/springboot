0d6ef396cc25855afbfb4630fe8c00544ef822aa
JONGHUKIM <hotsky803@gmail.com>
2025-02-27 10:41:07 +0900

    .

---
+++ springboot4/src/main/java/com/itwill/springboot4/dto/CommentRegisterDto.java
@@ -0,0 +1,16 @@
+package com.itwill.springboot4.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRegisterDto {
+
+	private String text;
+	private String writer;
+	private Long postId;
+	
+}
